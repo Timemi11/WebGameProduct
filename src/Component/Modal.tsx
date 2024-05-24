3ab43845133ff588bb938a2f5908789fb7a79992
Timemi11 <momuu56@gmail.com>
2024-05-24 13:24:14 +0700

    .

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -1,8 +1,6 @@
-import React, { useContext, useEffect } from 'react';
-import { GameProduct } from './Model/GameProduct';
-import liff from '@line/liff';
-
-
+import React, { useContext, useEffect } from "react";
+import { GameProduct } from "./Model/GameProduct";
+import liff from "@line/liff";
 
 type ModalProps = {
   handleToggleModal: () => void;
@@ -10,75 +8,154 @@ type ModalProps = {
 };
 
 export default function Modal({ handleToggleModal, product }: ModalProps) {
-  const liffId = '2005244347-lY246dm4';
+  const liffId = "2005244347-lY246dm4";
 
   // userId Uee534050cb274b81e66a9f0333932612
 
   const sendMessageToLine = async () => {
     try {
       const response = await fetch("https://api.line.me/v2/bot/message/push", {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           // Bearer ต่อด้วย  Channel access token ของ messagesing api
           Authorization:
-            'Bearer eCR3NwXUmzIqOq8HMYtuXooaWPDEBlszMMeF6BGoyRk4XpK2Ho89HV+hF0IUBuhsTRZYhWxLzRPFV6GyywHaaY7EL4t6uH8KgWUDTh4crPqW560gTHNJC98g+eStkQXgxKUO5StidnjRdPDxScYUHAdB04t89/1O/w1cDnyilFU=',
+            "Bearer eCR3NwXUmzIqOq8HMYtuXooaWPDEBlszMMeF6BGoyRk4XpK2Ho89HV+hF0IUBuhsTRZYhWxLzRPFV6GyywHaaY7EL4t6uH8KgWUDTh4crPqW560gTHNJC98g+eStkQXgxKUO5StidnjRdPDxScYUHAdB04t89/1O/w1cDnyilFU=",
         },
         body: JSON.stringify({
-          // to : userId
-          to: 'Uee534050cb274b81e66a9f0333932612',
+          to: "Uee534050cb274b81e66a9f0333932612",
           messages: [
             {
-              type: 'text',
-              text: 'ส่งข้อความ',
+              type: "flex",
+              altText: "GameProduct Sent",
+              contents: {
+                type: "bubble",
+                hero: {
+                  type: "image",
+                  size: "full",
+                  aspectRatio: "20:13",
+                  aspectMode: "cover",
+                  action: {
+                    type: "uri",
+                    uri: "https://line.me/",
+                  },
+                  url: "https://th.bing.com/th/id/OIP.rpNcXv9QqIj19FYeFWoayQHaKq?rs=1&pid=ImgDetMain",
+                },
+                body: {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "Mass Effect",
+                      weight: "bold",
+                      size: "xl",
+                      align: "center",
+                    },
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "text",
+                          text: "ราคา 520 บาท",
+                          align: "center",
+                          color: "#A1DD70",
+                        },
+                      ],
+                    },
+                  ],
+                },
+                footer: {
+                  type: "box",
+                  layout: "vertical",
+                  spacing: "md",
+                  contents: [
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "button",
+                          action: {
+                            type: "uri",
+                            label: "ยืนยันการสั่งซื้อ",
+                            uri: "http://linecorp.com/",
+                          },
+                          color: "#ffffff",
+                        },
+                      ],
+                      margin: "sm",
+                      backgroundColor: "#1B1A55",
+                      cornerRadius: "xl",
+                    },
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "button",
+                          action: {
+                            type: "uri",
+                            label: "เว็บของเรา",
+                            uri: "http://linecorp.com/", //เชื่อมเว็บของ GameProduct
+                          },
+                          color: "#ffffff",
+                        },
+                      ],
+                      backgroundColor: "#535C91",
+                      cornerRadius: "xl",
+                    },
+                  ],
+                  flex: 0,
+                  cornerRadius: "none",
+                },
+              },
             },
           ],
         }),
       });
 
       if (response.ok) {
-        alert('Message sent successfully');
+        alert("Message sent successfully");
       } else {
-        alert('Failed to send message');
+        alert("Failed to send message");
       }
     } catch (error) {
-      console.error('Failed to send message:', error);
-      alert('Failed to send message');
+      console.error("Failed to send message:", error);
+      alert("Failed to send message");
     }
   };
 
   const logInBeforeBuy = () => {
-      liff
-        .init({
-          liffId: liffId,
-        })
-        .then(() => {
-          if(!liff.isLoggedIn()){
-            liff.login();
-          }else{
-            sendMessageToLine()
-          }
-           });
+    liff
+      .init({
+        liffId: liffId,
+      })
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          sendMessageToLine();
+        }
+      });
   };
 
   return (
-    <div  
-      className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div
         className="overflow-y-auto text-white flex flex-col items-center p-8 rounded-lg max-w-lg w-full"
         style={{
-          maxHeight: '-webkit-fill-available',
-          backgroundColor:'#212233',
-          scrollbarWidth: 'thin',
-          scrollbarColor:'#AAADBE #6842ff'
-        }}
-      >
+          maxHeight: "-webkit-fill-available",
+          backgroundColor: "#212233",
+          scrollbarWidth: "thin",
+          scrollbarColor: "#AAADBE #6842ff",
+        }}>
         <h2 className="text-2xl font-bold mb-4">{product.prod_name}</h2>
         <img
           src={product.prod_img}
           alt="prod_img"
-          className=" h-48 w-48 object-cover mb-4 rounded-lg"
-        ></img>
+          className=" h-48 w-48 object-cover mb-4 rounded-lg"></img>
         <p>รายละเอียด</p>
         <p className="text-white mb-4">{product.prod_desc}</p>
         <p className="mb-4 text-green-500 text-xl font-extrabold">
@@ -87,14 +164,12 @@ export default function Modal({ handleToggleModal, product }: ModalProps) {
         <div className="buttonGroup flex gap-x-6 justify-between w-full">
           <button
             onClick={handleToggleModal}
-            className="w-1/2 mt-4 text-2xl font-extrabold bg-red-500 hover:bg-red-600 text-white px-8 py-4 rounded-md"
-          >
+            className="w-1/2 mt-4 text-2xl font-extrabold bg-red-500 hover:bg-red-600 text-white px-8 py-4 rounded-md">
             ปิด
           </button>
           <button
             onClick={logInBeforeBuy}
-            className="w-1/2 mt-4 text-2xl font-extrabold bg-green-500 hover:bg-green-600 text-white px-8 py-4 rounded-md"
-          >
+            className="w-1/2 mt-4 text-2xl font-extrabold bg-green-500 hover:bg-green-600 text-white px-8 py-4 rounded-md">
             สั่งซื้อ
           </button>
         </div>
