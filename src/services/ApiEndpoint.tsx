0b5bbc2741488e896a60dbc68aa6ab25681d3ed3
time <chatchawan.aromtip@gmail.com>
2024-06-10 17:42:48 +0700

    ...

--- src/services/ApiEndpoint.tsx
+++ src/services/ApiEndpoint.tsx
@@ -1,8 +1,8 @@
-export const endpoint = "https://archive-rouge-five.vercel.app";
+export const endpoint = "https://back-endof-web-game-main.vercel.app";
 export const endpointSteam = "https://steam-api-psi.vercel.app";
 export const steamUrlGame = "https://store.steampowered.com/app/";
 
 // http://localhost:8080
-// https://archive-rouge-five.vercel.app
+// https://back-endof-web-game-main.vercel.app
 
 // https://store.steampowered.com/app/id
