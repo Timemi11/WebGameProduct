86e787d4489b0f8b840f8d86581a96bc043d6fb4
640710561 ลัทธวิทย์ พักตระเกษตริน <paktraketrin_l@silpakorn.edu>
2024-06-12 11:50:04 +0700

    add FavButton

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -4,11 +4,16 @@ import { GameInfo, SteamGame } from "../type/Items";
 import { getFeatureGameSteam } from "../services/HttpMethod";
 import { steamUrlGame } from "../services/ApiEndpoint";
 import GameCarousel from "./GameCarosel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 
 export default function ShowGameProduct() {
   const [isDetail, setIsDetail] = useState<boolean>(false);
   const [selectedProduct, setSelectedProduct] = useState<GameInfo>();
   const [gameSteam, setGameSteam] = useState<SteamGame | undefined>(undefined);
+  const [favorites, setFavorites] = useState<Set<number>>(new Set());
+  
   const handleToggleModal = (product?: GameInfo) => {
     setIsDetail(!isDetail);
     setSelectedProduct(product);
@@ -18,19 +23,32 @@ export default function ShowGameProduct() {
     const steamGame = (await getFeatureGameSteam()) || undefined;
     setGameSteam(steamGame);
   }
-  console.log(gameSteam);
-
+  
   useEffect(() => {
     get();
   }, []);
 
+  const handleFavorite = (item: GameInfo) => {
+    console.log("Favorited:", item.id);
+    setFavorites(prev => {
+      const newFavorites = new Set(prev);
+      if (newFavorites.has(item.id)) {
+        newFavorites.delete(item.id);
+      } else {
+        newFavorites.add(item.id);
+      }
+      console.log("Set:", newFavorites);
+      return newFavorites;
+    });
+  };
+
   return (
-    <div className=" container mx-auto p-8 ">
+    <div className="container mx-auto p-8">
       <h1
         style={{
           backgroundColor: "rgb(104, 66, 255)",
         }}
-        className="text-3xl text-white text-center shadow-md p-4 rounded-lg  font-semibold mb-8 ">
+        className="text-3xl text-white text-center shadow-md p-4 rounded-lg font-semibold mb-8">
         เกมแนะนำ
       </h1>
       <GameCarousel gameSteam={gameSteam} steamUrlGame={steamUrlGame} />
@@ -38,28 +56,32 @@ export default function ShowGameProduct() {
         style={{
           backgroundColor: "rgb(104, 66, 255)",
         }}
-        className="text-3xl text-white text-center shadow-md p-4 rounded-lg  font-semibold my-8 ">
+        className="text-3xl text-white text-center shadow-md p-4 rounded-lg font-semibold my-8">
         เกมล่าสุด
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center gap-8 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center gap-8">
         {gameSteam?.featured_mac.map((items, ind) => (
           <div
             key={ind}
-            className=" flex flex-col items-center justify-center text-white p-4 shadow-2xl rounded-lg ">
-            <div className="image w-46 h-full mb-4 flex justify-center items-center">
+            className="flex flex-col items-center justify-center text-white p-4 shadow-2xl rounded-lg relative">
+            <div className="image w-46 h-full mb-4 flex justify-center items-center relative">
               <img
                 src={items.large_capsule_image}
                 alt="prod_img"
                 onClick={() => handleToggleModal(items)}
                 className="object-cover rounded-lg"></img>
+              <button
+                onClick={() => handleFavorite(items)}
+                className={`absolute top-1 right-1 p-1`}>
+                <FontAwesomeIcon icon={favorites.has(items.id) ? solidHeart : regularHeart} />
+              </button>
             </div>
             <div
               style={{ backgroundColor: "#212233" }}
               className="details w-full h-full flex flex-col justify-center items-center text-center p-4 rounded-lg">
-              <h3 className="text-2xl font-semibold text-center ">
+              <h3 className="text-2xl font-semibold text-center">
                 {items.name}
               </h3>
-
               {items.discount_percent === 0 ? (
                 <p className="text-green-400 text-xl">
                   {items.original_price === 0 || items.original_price === null
@@ -67,7 +89,7 @@ export default function ShowGameProduct() {
                     : "ราคา " + items.original_price / 100}
                 </p>
               ) : (
-                <div className="flex flex-col ">
+                <div className="flex flex-col">
                   <p className="text-red-700 line-through">
                     ราคาเดิม {items.original_price / 100} บาท
                   </p>
@@ -76,7 +98,6 @@ export default function ShowGameProduct() {
                   </p>
                 </div>
               )}
-
               <button
                 onClick={() => handleToggleModal(items)}
                 className="mt-4 font-extrabold bg-violet-800 hover:bg-violet-700 text-white px-4 py-2 rounded-md">
@@ -85,7 +106,7 @@ export default function ShowGameProduct() {
             </div>
           </div>
         ))}
-        {/*  เปิด modal */}
+        {/* เปิด modal */}
         {isDetail && selectedProduct && (
           <Modal
             handleToggleModal={handleToggleModal}
