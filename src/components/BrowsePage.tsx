748536a625c4ab8dcc8ca1342e4a8b4b864637ed
Phunsup S <avg.artvengers@gmail.com>
2024-06-12 16:38:54 +0700

    Log WhistList

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -43,6 +43,7 @@ export default function ShowGameProduct() {
     if (arrApp === undefined) {
       console.log("create");
       setWishList([...wishList,info]);
+      console.log(wishList)
       await updateWishlist(info as Wishlist, dataLine?.userId || "");
     } else {
       // if มี appid ก็ไม่ต้องเพิ่ม
