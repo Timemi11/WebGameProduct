a60786d321a0cec0cd3883cea59a75081c30cdd2
Timemi11 <momuu56@gmail.com>
2024-06-13 23:07:09 +0700

    depen

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -117,7 +117,7 @@ export default function ShowGameProduct() {
     }
 
     // ใช้wishlist แล้วมันนำข้อมูล่าสุดมาให้ หลัง dom render เสร้จ useeffect  // ! ไปดูต่อคืนนี้ lifecycle
-  }, [dataLine]);
+  }, [dataLine, wishList]);
 
   return (
     <div className=" container mx-auto p-8 ">
